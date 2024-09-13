885984789e337b6e1d30203095eeb9eaf363bdc1
MarlonDev <marlonpondavilla.com>
2024-09-13 23:36:56 +0800

    Get the address of the device being tracked

--- app/src/main/java/bsu/meneses/it304busreservationandtracking/MainActivity.java
+++ app/src/main/java/bsu/meneses/it304busreservationandtracking/MainActivity.java
@@ -5,6 +5,8 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
@@ -23,9 +25,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public static final int DEFAULT_UPDATE_INTERVAL = 30;
@@ -40,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
 
     //all config files ng fused
     LocationRequest locationRequest;
+
+    LocationCallback locationCallback;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -58,19 +66,31 @@ public class MainActivity extends AppCompatActivity {
         swGPS = findViewById(R.id.sw_gps);
 
         locationRequest = new LocationRequest();
-        locationRequest.setInterval(30000);
-        locationRequest.setFastestInterval(5000);
+        locationRequest.setInterval(1000 * DEFAULT_UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(1000 * FAST_UPDATE_INTERVAL);
         locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        //event triggered whenever update is met
+        locationCallback = new LocationCallback() {
+
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+                //save location
+                Location location = locationResult.getLastLocation();
+                updateUIValues(location);
+            }
+        };
 
 
         swGPS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(swGPS.isChecked()){
+                if (swGPS.isChecked()) {
                     locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
                     swGPS.setText("Using GPS sensors");
-                }
-                else{
+                } else {
                     locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
                     swGPS.setText("off");
                 }
@@ -78,9 +98,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        fusedLocationProviderClient  = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             //user accepted the permission
 
             fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
@@ -91,17 +111,58 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             });
-        }
-        else{
+        } else {
             //permission not granted yet
 
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_FINE_LOCATION);
             }
         }
 
+        swUpdates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (swUpdates.isChecked()) {
+                    //turn on tracking
+                    startLocationUpdates();
+                } else {
+                    //turn off tracking
+                    stopLocationUpdates();
+                }
+            }
+        });
+
         updateGPS();
     }//on create method end bracket
+
+    private void startLocationUpdates() {
+        swUpdates.setText("location is being tracked");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+        updateGPS();
+    }
+
+
+    private void stopLocationUpdates() {
+        swUpdates.setText("location is NOT being tracked");
+        lati.setText("Not tracking location");
+        longi.setText("Not tracking location");
+        accuracy.setText("Not tracking location");
+        speed.setText("Not tracking location");
+        alt.setText("Not tracking location");
+        address.setText("Not tracking location");
+
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -119,6 +180,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
+
 
     private void updateGPS(){
 
@@ -141,6 +203,20 @@ public class MainActivity extends AppCompatActivity {
         }
         else{
             speed.setText("Not available");
+        }
+
+        //this is it pancit shit
+        Geocoder geocoder = new Geocoder(MainActivity.this);
+
+        try{
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+            //change this na lang if we'll get other location stuff
+            address.setText(addresses.get(0).getAddressLine(0) + ", " + addresses.get(0));
+        }
+        catch (Exception e){
+            address.setText("Unable to get street adress");
+            Toast.makeText(this, "Cannot provide the street address", Toast.LENGTH_SHORT).show();
         }
 
     }
