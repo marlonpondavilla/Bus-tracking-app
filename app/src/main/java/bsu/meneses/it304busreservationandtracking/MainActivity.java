fca8ec476cf51e3bf53a8a143c4aa0f98a716d02
MarlonDev <marlonpondavilla.com>
2024-09-14 09:30:43 +0800

    refactor redundant code

--- app/src/main/java/bsu/meneses/it304busreservationandtracking/MainActivity.java
+++ app/src/main/java/bsu/meneses/it304busreservationandtracking/MainActivity.java
@@ -138,13 +138,6 @@ public class MainActivity extends AppCompatActivity {
     private void startLocationUpdates() {
         swUpdates.setText("location is being tracked");
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
         fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
