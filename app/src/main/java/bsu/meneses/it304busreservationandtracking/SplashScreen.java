7a65addb3412be00f9d4442fa36220c2a0bd6cfc
Darwin <Darwin.com>
2024-10-03 07:31:26 +0800

    Integration of splash Screen and welcome page

---
+++ app/src/main/java/bsu/meneses/it304busreservationandtracking/SplashScreen.java
@@ -0,0 +1,37 @@
+package bsu.meneses.it304busreservationandtracking;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Delayed transition to MainActivity
+        handler.postDelayed(() -> {
+            Log.d("SplashScreen", "Navigating to MainActivity");
+            Intent intent = new Intent(SplashScreen.this, WelcomePage.class);
+            startActivity(intent);
+            finish();
+        }, 4000);
+    }
+}
